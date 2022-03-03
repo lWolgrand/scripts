bacdc58828556e372a1f8963f53eb89af61c4482
lWolgrand <lopeswolgrand@gmail.com>
2022-03-02 23:18:31 -0300

    Repo para scripts

---
+++ testes-script/apps.sh
@@ -0,0 +1,6 @@
+#Instalando chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+
+#Instalando VsCode
+sudo snap install code --classic
